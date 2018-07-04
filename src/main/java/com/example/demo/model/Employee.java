969334fbb08e112814078c5b9b9f59643d988fb0
kehwang <kehwang@LNAR-PF12V7XY.corp.capgemini.com>
2018-07-04 11:38:10 -0400

    modeling

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,73 @@
+package com.example.demo.model;
+
+public class Employee {
+	int employeeID;
+	String firstname;
+	String lastname;
+	String email;
+	String phone;
+	
+	
+	//Constructor
+	public Employee(int employeeID, String firstname, String lastname, String email, String phone) {
+		super();
+		this.employeeID = employeeID;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.phone = phone;
+	}
+
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/model/WorkExperience.java
@@ -0,0 +1,7 @@
+package com.example.demo.model;
+
+public class WorkExperience {
+	
+	int employeeID;
+	int workExperienceID;
+}
