8723e8a9ef41075ff6accaa7ad3fa262aa498a55
Louis Liu <louisliu15@outlook.com>
2018-07-09 13:55:26 -0400

    save and delete temApp

--- src/main/java/com/example/demo/service/TemAppServiceImpl.java
+++ src/main/java/com/example/demo/service/TemAppServiceImpl.java
@@ -8,21 +8,27 @@ import java.util.TreeMap;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Application;
 import com.example.demo.model.TemApplication;
+import com.example.demo.repository.AppRepository;
 import com.example.demo.repository.TemAppRepository;
 import com.example.utils.TypeConvertUtils;
 
+@Service
 public class TemAppServiceImpl implements TemAppService {
-	
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Autowired
 	private TemAppRepository tRepo;
+	
+	@Autowired
+	private AppRepository aRepo;
 
 	@Override
 	public boolean hasTemApplicationService(int employeeID) {
@@ -63,16 +69,21 @@ public class TemAppServiceImpl implements TemAppService {
 	}
 	
 	@Override
+	@Transactional
 	public boolean saveTemApplicationService(int employeeID, Application app) {
-		// TODO Auto-generated method stub
+		if(aRepo.findById(employeeID)!=null) {
+			tRepo.save(TypeConvertUtils.applcationToTemApplication(employeeID, app));
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean deleteTemApplicationService(int employeeID, int temAppID) {
-		// TODO Auto-generated method stub
+		if(aRepo.findById(employeeID)!=null) {
+			tRepo.deleteById(temAppID);
+			return true;
+		}
 		return false;
 	}
-	
-
 }
