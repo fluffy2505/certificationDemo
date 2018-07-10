073b0744d69e9335717746d4ba2f107584827f25
snepatel <snepatel@LNAR-F29YYF2.corp.capgemini.com>
2018-07-10 13:40:13 -0400

    Revert "test"
    This reverts commit 848eb4af4f152b272327ef4c907ef6c9a7695b67.

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
