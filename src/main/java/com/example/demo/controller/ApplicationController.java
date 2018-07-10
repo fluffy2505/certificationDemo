1f70db94368dcc645dee52e22a7ea59327232dec
snepatel <snepatel@LNAR-F29YYF2.corp.capgemini.com>
2018-07-10 15:19:51 -0400

    Revert "Home Page linked to Login"
    This reverts commit b1d92ef91043e4c8da6ef2b7da3fa30459bd6387.

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -2,24 +2,16 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ApplicationController {
 
-	@RequestMapping("/")
-	public String sayHello(Model model ) {
+	@RequestMapping("/hello")
+	public String sayHello(Model model) {
 	
 		model.addAttribute("hello", "application controller say hello");
-		return "index";
+		return "hello";
 	}
 	
-	
-	@RequestMapping(value="/{arg}", method=RequestMethod.GET)
-	public String sayHello(@PathVariable("arg") String arg) {
-		arg = "Login";
-		return arg;
-	}
 }
