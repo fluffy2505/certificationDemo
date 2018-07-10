971695d23930632d7b50e0f6f4662cd59a4b3601
snepatel <snepatel@LNAR-F29YYF2.corp.capgemini.com>
2018-07-10 15:31:33 -0400

    HomePage and Login linked

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -2,16 +2,24 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ApplicationController {
 
-	@RequestMapping("/hello")
-	public String sayHello(Model model) {
+	@RequestMapping("/")
+	public String sayHello(Model model ) {
 	
 		model.addAttribute("hello", "application controller say hello");
-		return "hello";
+		return "index";
 	}
 	
+	
+	@RequestMapping(value="/{arg}", method=RequestMethod.GET)
+	public String sayHello(@PathVariable("arg") String arg) {
+		arg = "Login";
+		return arg;
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+	<h1>Home Page</h1>
+	<h3><a href="<c:url value="/{arg}" /> ">Click me to login!</a></h3>
+
+</body>
+</html>
