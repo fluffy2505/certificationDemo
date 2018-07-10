143d137264c89e5e04cc2b4c57ebe6b310db37c6
snepatel <snepatel@LNAR-F29YYF2.corp.capgemini.com>
2018-07-10 14:00:32 -0400

    ApplicationController

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ApplicationController {
 
+	@RequestMapping("/hello")
+	public String sayHello(Model model) {
+	
+		model.addAttribute("hello", "application controller say hello");
+		return "hello";
+	}
+	
 }
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -6,6 +6,8 @@
 <meta charset="UTF-8">
 <title>Hello</title>
 </head>
-<body>Hello ${name}
+<body>
+
+	<h1>${hello}</h1>
 </body>
 </html>
